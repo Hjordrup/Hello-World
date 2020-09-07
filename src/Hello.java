4e5f0c515197bb308d8ce4d99dc9fc4bfaae5a05
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2020-09-07 11:37:27 +0200

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+public class Hello {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello World!");
+        System.out.println("Dette skal være den anden linje");
+        System.out.println("Jeg ved ikke hvad jeg skal skrive i den trejde linje med dette vil være den trejde linje som skal printes til min consol");
+        System.out.println(5);
+    }
+}
